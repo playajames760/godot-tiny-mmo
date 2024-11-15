53b44edaeb86f52e3c994d3968f8e5ca02d004e2
SlayHorizon <163869998+SlayHorizon@users.noreply.github.com>
2024-11-15 12:04:12 +0100

    Use world-server instead of game-server

--- source/common/main.gd
+++ source/common/main.gd
@@ -6,20 +6,16 @@ extends Node
 func _ready() -> void:
 	if OS.has_feature("client"):
 		start_as_client()
-	elif OS.has_feature("game-server"):
-		start_as_game_server()
 	elif OS.has_feature("gateway-server"):
 		start_as_gateway_server()
 	elif OS.has_feature("master-server"):
 		start_as_master_server()
+	elif OS.has_feature("world-server"):
+		start_as_world_server()
 
 
 func start_as_client() -> void:
 	get_tree().change_scene_to_file.call_deferred("res://source/client/client_main.tscn")
-
-
-func start_as_game_server() -> void:
-	get_tree().change_scene_to_file.call_deferred("res://source/world_server/world_main.tscn")
 
 
 func start_as_gateway_server() -> void:
@@ -28,3 +24,7 @@ func start_as_gateway_server() -> void:
 
 func start_as_master_server() -> void:
 	get_tree().change_scene_to_file.call_deferred("res://source/master_server/master_main.tscn")
+
+
+func start_as_world_server() -> void:
+	get_tree().change_scene_to_file.call_deferred("res://source/world_server/world_main.tscn")
