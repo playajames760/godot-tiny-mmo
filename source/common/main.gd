bac5b009f427630f17a05af9f650e10bac795b34
SlayHorizon <163869998+SlayHorizon@users.noreply.github.com>
2024-11-24 17:06:41 +0100

    Update main

--- source/common/main.gd
+++ source/common/main.gd
@@ -1,7 +1,8 @@
 extends Node
-## Main.
-## Should only care about deciding if the project is either
-## a world server, gateway server, master server or a client.
+## Main Scene - This is the entry point of the application (set at application/run/main_scene).
+## Its sole responsibility is to determine and initialize the application role
+## (world server, gateway server, master server, or client) based on feature flags.
+
 
 func _ready() -> void:
 	if OS.has_feature("client"):
@@ -12,6 +13,11 @@ func _ready() -> void:
 		start_as_master_server()
 	elif OS.has_feature("world-server"):
 		start_as_world_server()
+	else:
+		printerr(
+			"No valid feature tag was found."
+			+ "\nPlease check either README.md or common/main.gd."
+		)
 
 
 func start_as_client() -> void:
