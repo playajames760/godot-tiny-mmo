0c01043a4d998e9c5b6548d6d7d31bb7cd48ce47
SlayHorizon <163869998+SlayHorizon@users.noreply.github.com>
2025-03-03 23:18:52 +0100

    Improve character creation menu

--- source/client/gateway/ui/components/character_creation/button/character_class_button.gd
+++ source/client/gateway/ui/components/character_creation/button/character_class_button.gd
@@ -3,6 +3,8 @@ class_name CharacterClassButton
 extends Button
 
 
+const SELECT_STYLEBOX = preload("res://source/client/gateway/ui/components/character_creation/button/select_stylebox.tres")
+
 @export var character_class: CharacterResource:
 	set(value):
 		if not value:
@@ -25,3 +27,13 @@ func _ready() -> void:
 func update_button() -> void:
 	label.text = character_class.character_name
 	animated_sprite_2d.sprite_frames = character_class.character_sprite
+
+
+func apply_select_style() -> void:
+	add_theme_stylebox_override(&"normal", SELECT_STYLEBOX)
+	add_theme_stylebox_override(&"hover", SELECT_STYLEBOX)
+
+
+func remove_select_style() -> void:
+	remove_theme_stylebox_override(&"normal")
+	remove_theme_stylebox_override(&"hover")
--- source/client/gateway/ui/components/character_creation/character_creation.gd
+++ source/client/gateway/ui/components/character_creation/character_creation.gd
@@ -1,9 +1,10 @@
 extends GatewayUIComponent
 
 
-
 @export var class_description: Label
+@export var default_selection: CharacterClassButton
 
+var selected_button: CharacterClassButton = null
 var selected_character_class: CharacterResource = preload("res://source/common/resources/custom/character/character_collection/knight.tres")
 
 @onready var class_selection_container: VBoxContainer = $CenterContainer/VBoxContainer/HBoxContainer/VBoxContainer
@@ -15,54 +16,76 @@ var selected_character_class: CharacterResource = preload("res://source/common/r
 
 
 func _ready() -> void:
+	visibility_changed.connect(
+		func():
+			if visible:
+				default_selection.grab_focus.call_deferred()
+	)
+	
+	if default_selection:
+		default_selection.apply_select_style()
+		selected_button = default_selection
+		selected_character_class = default_selection.character_class
+	
 	create_character_button.disabled = true
+	update_character_preview()
 	
+	var buttons: Array[Button]
+	buttons.assign(find_children("*", "CharacterClassButton", true))
+	connect_character_class_buttons(buttons)
+	ButtonUtils.set_focus_neighbors_for_buttons(buttons)
+
+
+func update_character_preview() -> void:
 	character_preview.sprite_frames = selected_character_class.character_sprite
-	update_character_class_description()
 	character_preview.play(&"idle")
+	class_description.text = "%s\n%s" % [
+		selected_character_class.character_name,
+		selected_character_class.description
+	]
+
+
+func connect_character_class_buttons(buttons: Array[Button]) -> void:
+	for button: CharacterClassButton in buttons:
+		button.pivot_offset = button.size / 2
+		button.pressed.connect(_on_character_class_button_pressed.bind(button))
+		button.mouse_entered.connect(button.grab_focus)
+		button.focus_entered.connect(_animate_button.bind(button))
+		button.focus_exited.connect(_reset_animation.bind(button))
+
+
+func _on_character_class_button_pressed(button: CharacterClassButton) -> void:
+	selected_character_class = button.character_class
+	if selected_button:
+		selected_button.remove_select_style()
+	selected_button = button
+	button.apply_select_style()
+	update_character_preview()
+
+
+func _animate_button(button: CharacterClassButton) -> void:
+	var tween := create_tween()
+	var target_scale := Vector2.ONE * 1.1
 	
-	connect_class_buttons()
+	tween.tween_property(button, "scale", target_scale, 0.15) \
+		.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(button, "scale", Vector2.ONE, 0.2) \
+		.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	
+	button.animated_sprite_2d.play(&"run")
+
+
+func _reset_animation(button: CharacterClassButton) -> void:
+	button.animated_sprite_2d.play(&"idle")
 
 
 func generate_random_username() -> String:
 	var characters := "abcdefghijklmnopqrstuvwxyz0123456789"
 	var username := ""
 	for i in range(8):
-		username += characters[randi()% len(characters)]
+		username += characters[randi() % len(characters)]
 	return username
 
-
-func connect_class_buttons() -> void:
-	for child in class_selection_container.get_children():
-		if child is CharacterClassButton:
-			child.pivot_offset = child.size / 2
-			child.pressed.connect(_on_character_class_button_pressed.bind(child.character_class))
-			child.mouse_entered.connect(
-				func():
-					var tween := create_tween()
-					tween.tween_property(child, "scale", Vector2(1.2, 1.2), 0.2) \
-						.from(Vector2.ONE)
-					child.get_node("CenterContainer/Control/AnimatedSprite2D").play("run")
-			)
-			child.mouse_exited.connect(
-				func():
-					var tween := create_tween()
-					tween.tween_property(child, "scale", Vector2.ONE, 0.2) \
-						.from_current()
-					if not child.has_focus():
-						child.get_node("CenterContainer/Control/AnimatedSprite2D").play("idle")
-			)
-			child.focus_exited.connect(
-				func():
-					child.get_node("CenterContainer/Control/AnimatedSprite2D").play("idle")
-			)
-
-
-func _on_character_class_button_pressed(character_class: CharacterResource) -> void:
-	selected_character_class = character_class
-	update_character_class_description()
-	character_preview.sprite_frames = character_class.character_sprite
-	character_preview.play(&"idle") 
 
 func _on_rng_button_pressed() -> void:
 	username_edit.text = generate_random_username()
@@ -102,10 +125,3 @@ func _on_create_character_button_pressed() -> void:
 		},
 		gateway.world_id
 	)
-
-
-func update_character_class_description() -> void:
-	class_description.text = "%s\n%s" % [
-		selected_character_class.character_name,
-		selected_character_class.description
-	]
