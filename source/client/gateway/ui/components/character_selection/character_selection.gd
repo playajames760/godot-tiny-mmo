b8a8866224eadd457591a8a7d6ee78a7a5eb804f
SlayHorizon <163869998+SlayHorizon@users.noreply.github.com>
2025-04-06 12:01:41 +0200

    Improve character selection menu

--- source/client/gateway/ui/components/character_selection/character_selection.gd
+++ source/client/gateway/ui/components/character_selection/character_selection.gd
@@ -7,19 +7,26 @@ const CHARACTER_SLOT_BUTTON = preload("res://source/client/gateway/ui/components
 @export var next_menu: Control
 
 @onready var slots_container: HBoxContainer = $CenterContainer/HBoxContainer
-
+@onready var create_new_character_button: Button = $CreateNewCharacter
 @onready var waiting_rect: ColorRect = $CenterContainer/WaitingRect
 
 
 func _ready() -> void:
+	create_new_character_button.hide()
+	create_new_character_button.pressed.connect(_on_character_slot_button_pressed.bind(create_new_character_button.text))
 	for placeholder in slots_container.get_children():
 		placeholder.queue_free()
 
 
 func set_player_characters(player_characters: Dictionary) -> void:
-	var new_character_slot := CHARACTER_SLOT_BUTTON.instantiate()
-	new_character_slot.pressed.connect(_on_character_slot_button_pressed.bind(new_character_slot.text))
-	slots_container.add_child(new_character_slot)
+	var new_character_slot: Button = CHARACTER_SLOT_BUTTON.instantiate()
+	
+	if player_characters.is_empty():
+		new_character_slot.pressed.connect(_on_character_slot_button_pressed.bind(new_character_slot.text))
+		slots_container.add_child(new_character_slot)
+	else:
+		create_new_character_button.show()
+	
 	for character_id in player_characters:
 		new_character_slot = CHARACTER_SLOT_BUTTON.instantiate()
 		new_character_slot.text = "%s\nClass: %s\nLevel: %d" % [
@@ -39,7 +46,8 @@ func set_player_characters(player_characters: Dictionary) -> void:
 func _on_character_slot_button_pressed(text: String, character_id: int = 0) -> void:
 	for button: Button in slots_container.get_children():
 		button.disabled = true
-	if text == "Empty character slot": 
+	create_new_character_button.disabled = true
+	if text == "Create New Character": 
 		hide()
 		next_menu.show()
 	else:
